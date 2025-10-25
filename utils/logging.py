65099c88fa98bf59d395a916b83a65c27df2818c
loucs1546 <lcagnioncle@gmail.com>
2025-10-26 00:58:02 +0200

    Create logging.py

---
+++ utils/logging.py
@@ -0,0 +1,13 @@
+import discord
+import config
+
+async def send_log(bot, channel_key: str, embed: discord.Embed):
+    channel_id = config.LOG_CHANNELS.get(channel_key)
+    if not channel_id:
+        return
+    channel = bot.get_channel(channel_id)
+    if channel:
+        try:
+            await channel.send(embed=embed)
+        except Exception:
+            pass
